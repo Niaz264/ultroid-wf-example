3be1e7d7b6a40445a96416f04f8174e1201de91a
DragonPower84 <patrobighnesh8@gmail.com>
2022-02-11 22:02:54 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,6 +1,6 @@
-branch=main
-git clone -b $branch https://github.com/TeamUltroid/Ultroid /root/TeamUltroid
-cp ultroid/.env /root/TeamUltroid/.env
-cd /root/TeamUltroid
-docker build . --rm --force-rm --compress --pull --file Dockerfile -t ultroid
-docker run --privileged --env-file .env --rm -i ultroid
+branch=master
+git clone -b $branch https://github.com/UsergeTeam/Userge /root/UsergeTeam
+cp ultroid/.env /root/UsergeTeam/config.env
+cd /root/UsergeTeam
+docker build . --rm --force-rm --compress --pull --file Dockerfile -t kai
+docker run --privileged --env-file config.env --rm -i kai
